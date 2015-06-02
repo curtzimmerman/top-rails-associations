f31184ac50f1feeb3e962ebda437298097d2c2a5
Curt Zimmerman <czimmerman9@gmail.com>
2015-06-01 22:35:31 -0400

    add login functionality

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def full_title(page = '')
+		base_title = "EventPlanner"
+		if page.empty?
+			base_title
+		else
+			"#{page} | #{base_title}"
+		end
+	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,10 @@ Rails.application.routes.draw do
 
   resources :users
 
-  get 'signup' => 'users#new'
+  get 'signup'     => 'users#new'
+  get 'login'      => 'sessions#new'
+  post 'login'     => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
